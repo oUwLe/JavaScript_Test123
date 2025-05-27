51cc2511cf8462ff16d8efda39b31c8dc03de22c
oUwLe <sebastian.ljung18@hotmail.com>
2025-05-27 12:52:05 +0200

    made testy

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//två backslash för kommentar som inte kommer tolkas av webbläsaren som kod
+
+let myName = "Sebastian"; // String
+let age = 25; // Number
+const isStudent = true; // Boolean (true or false)
+
+console.log("Hej " + myName + " jag är " + age + " år ");
+console.log("Student: " + isStudent)
