ba929ec98e2645285624ff8d76b15169b365c6ae
oUwLe <sebastian.ljung18@hotmail.com>
2025-05-27 15:48:23 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,30 @@
 //två backslash för kommentar som inte kommer tolkas av webbläsaren som kod
 
-let myName = "Sebastian";     // String
+const myName = "Sebastian";     // String
 let age = 25;                  // Number
 let isStudent = true;           // Boolean (true or false)
 
-console.log("Hej " + myName + " jag är " + age + " år ");
+console.log("Hej mitt namn är " + myName + " och jag är " + age + " år gammal och jag är student ");
 console.log("Student: " + isStudent)
 
-if (isStudent == true) {          // Två eller tre lika med tecken, inte ett
-    console.log('Success!');
+if (age > 18) {
+    console.log("Adult student")
 }
-
+if (age < 18) {
+    console.log("Under 18 student")
+}
+if (myName == "Sebastian" && age == 25) {
+    console.log("Identity confirmed!")
+}
+if (isStudent == true && age > 17) {          // Två eller tre lika med tecken, inte ett
+    console.log('Log in accepted!');
+}
 else {  // <-- Detta bildar ett uttryck som kan vara sant eller falskt            
-    console.log('Failure!');   
+    console.log('Log in denied, student is under 18 or not registered as a student.');   
 }
 
-// Nu kan du ändra true or false på isStudent och den printar antingen sucess or failure.
+// > större än, < mindre än, 
+
+// == jämförelse, || eller, && och 
+
+// Nu kan du ändra true or false på isStudent och den visar log in denied eller accepted i konsol.
